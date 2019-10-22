837f56a4ff945be153fa6579922d07bc099fac5c
Restyled.io <commits@restyled.io>
2019-10-22 19:41:06 +0000

    Restyled by clang-format

--- WikipediaCleaner/src/org/wikipediacleaner/api/data/PageElementExternalLink.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/data/PageElementExternalLink.java
@@ -9,23 +9,22 @@ package org.wikipediacleaner.api.data;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.wikipediacleaner.api.constants.EnumWikipedia;
 import org.wikipediacleaner.api.data.contents.ContentsComment;
 
-
 /**
- * Class containing information about a complete external link ([http://... text]). 
+ * Class containing information about a complete external link ([http://...
+ * text]).
  */
 public class PageElementExternalLink extends PageElement {
 
   private final String linkNotTrimmed;
   private final String link;
-  private final int    linkBeginIndex;
-  private final int    linkEndIndex;
+  private final int linkBeginIndex;
+  private final int linkEndIndex;
   private final String textNotTrimmed;
   private final String text;
-  private final int    textOffset;
+  private final int textOffset;
   private final boolean hasSquare;
   private final boolean hasSecondSquare;
 
@@ -35,9 +34,11 @@ public class PageElementExternalLink extends PageElement {
 
   private final static String UNACCEPTABLE = "\n";
 
-  private final static String OUTSIDE_TEMPLATE_SEPARATORS = SEPARATORS_EXCLUDED + SEPARATORS_INCLUDED + UNACCEPTABLE + "[]";
+  private final static String OUTSIDE_TEMPLATE_SEPARATORS =
+      SEPARATORS_EXCLUDED + SEPARATORS_INCLUDED + UNACCEPTABLE + "[]";
 
-  private final static String IN_TEMPLATES_SEPARATORS = OUTSIDE_TEMPLATE_SEPARATORS + "|}";
+  private final static String IN_TEMPLATES_SEPARATORS =
+      OUTSIDE_TEMPLATE_SEPARATORS + "|}";
 
   private final static List<String> privateProtocols = new ArrayList<String>();
 
@@ -49,16 +50,16 @@ public class PageElementExternalLink extends PageElement {
 
   /**
    * Analyze contents to check if it matches an external link.
-   * 
+   *
    * @param wikipedia Wikipedia.
    * @param contents Contents.
    * @param index Block start index.
    * @param analysis Page analysis.
    * @return Block details it there's a block.
    */
-  public static PageElementExternalLink analyzeBlock(
-      EnumWikipedia wikipedia, String contents, int index,
-      PageAnalysis analysis) {
+  public static PageElementExternalLink analyzeBlock(EnumWikipedia wikipedia,
+                                                     String contents, int index,
+                                                     PageAnalysis analysis) {
     // Verify arguments
     if (contents == null) {
       return null;
@@ -78,8 +79,7 @@ public class PageElementExternalLink extends PageElement {
 
     // Possible white spaces characters
     if (hasSquare) {
-      while ((tmpIndex < maxLength) &&
-             (contents.charAt(tmpIndex) == ' ')) {
+      while ((tmpIndex < maxLength) && (contents.charAt(tmpIndex) == ' ')) {
         tmpIndex++;
       }
     }
@@ -113,26 +113,25 @@ public class PageElementExternalLink extends PageElement {
     }
 
     // Situations where the external link consists only of the URL
-    if (!hasSquare ||
-        (endUrlIndex >= maxLength) ||
+    if (!hasSquare || (endUrlIndex >= maxLength) ||
         (UNACCEPTABLE.indexOf(contents.charAt(endUrlIndex)) >= 0)) {
       return new PageElementExternalLink(
           beginUrlIndex, endUrlIndex,
-          contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex, endUrlIndex,
-          null, -1, false, false);
+          contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex,
+          endUrlIndex, null, -1, false, false);
     }
-    if ((endUrlIndex < maxLength) &&
-        (contents.charAt(endUrlIndex) == ']')) {
+    if ((endUrlIndex < maxLength) && (contents.charAt(endUrlIndex) == ']')) {
       return new PageElementExternalLink(
           index, endUrlIndex + 1,
-          contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex, endUrlIndex,
-          null, -1, true, true);
+          contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex,
+          endUrlIndex, null, -1, true, true);
     }
 
     // Compute maximum index for end of external link
     int maxEndIndex = maxLength;
     if (analysis != null) {
-      PageElementTag refTag = analysis.getSurroundingTag(PageElementTag.TAG_WIKI_REF, index);
+      PageElementTag refTag =
+          analysis.getSurroundingTag(PageElementTag.TAG_WIKI_REF, index);
       if ((refTag != null) && !refTag.isFullTag() && refTag.isComplete()) {
         maxEndIndex = refTag.getValueEndIndex();
       }
@@ -140,8 +139,9 @@ public class PageElementExternalLink extends PageElement {
 
     // Find beginning of text
     int beginTextIndex = endUrlIndex;
-    while ((beginTextIndex < maxEndIndex) &&
-           (SEPARATORS_EXCLUDED.indexOf(contents.charAt(beginTextIndex)) >= 0)) {
+    while (
+        (beginTextIndex < maxEndIndex) &&
+        (SEPARATORS_EXCLUDED.indexOf(contents.charAt(beginTextIndex)) >= 0)) {
       beginTextIndex++;
     }
 
@@ -156,33 +156,35 @@ public class PageElementExternalLink extends PageElement {
         }
         doubleSquareCount++;
         endTextIndex += 2;
-      } else if ((doubleSquareCount > 0) && contents.startsWith("]]", endTextIndex)) {
+      } else if ((doubleSquareCount > 0) &&
+                 contents.startsWith("]]", endTextIndex)) {
         doubleSquareCount--;
         endTextIndex += 2;
       } else if (contents.charAt(endTextIndex) == ']') {
         if (prematureEndIndex < 0) {
           return new PageElementExternalLink(
               index, endTextIndex + 1,
-              contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex, endUrlIndex,
-              contents.substring(beginTextIndex, endTextIndex),
+              contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex,
+              endUrlIndex, contents.substring(beginTextIndex, endTextIndex),
               beginTextIndex - index, true, true);
         }
         return new PageElementExternalLink(
             index, prematureEndIndex,
-            contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex, endUrlIndex,
-            contents.substring(beginTextIndex, prematureEndIndex),
+            contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex,
+            endUrlIndex, contents.substring(beginTextIndex, prematureEndIndex),
             beginTextIndex - index, true, false);
       } else if (UNACCEPTABLE.indexOf(contents.charAt(endTextIndex)) >= 0) {
         return new PageElementExternalLink(
             beginUrlIndex, endUrlIndex,
-            contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex, endUrlIndex,
-            null, -1, false, false);
+            contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex,
+            endUrlIndex, null, -1, false, false);
       } else {
         ContentsComment comment = null;
         PageElementTag tagNowiki = null;
         if ((contents.charAt(endTextIndex) == '<') && (analysis != null)) {
           comment = analysis.isInComment(endTextIndex);
-          tagNowiki = analysis.isInTag(endTextIndex, PageElementTag.TAG_WIKI_NOWIKI);
+          tagNowiki =
+              analysis.isInTag(endTextIndex, PageElementTag.TAG_WIKI_NOWIKI);
         }
         if (comment != null) {
           endTextIndex = comment.getEndIndex();
@@ -197,8 +199,8 @@ public class PageElementExternalLink extends PageElement {
     // No end found
     return new PageElementExternalLink(
         beginUrlIndex, endUrlIndex,
-        contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex, endUrlIndex,
-        null, -1, false, false);
+        contents.substring(beginUrlIndex, endUrlIndex), beginUrlIndex,
+        endUrlIndex, null, -1, false, false);
   }
 
   /**
@@ -213,7 +215,8 @@ public class PageElementExternalLink extends PageElement {
       while (same && (pos < protocol.length())) {
         if (offset + pos >= text.length()) {
           same = false;
-        } else if (protocol.charAt(pos) != Character.toLowerCase(text.charAt(offset + pos))) {
+        } else if (protocol.charAt(pos) !=
+                   Character.toLowerCase(text.charAt(offset + pos))) {
           same = false;
         } else {
           pos++;
@@ -229,44 +232,32 @@ public class PageElementExternalLink extends PageElement {
   /**
    * @return External link.
    */
-  public String getLink() {
-    return link;
-  }
+  public String getLink() { return link; }
 
   /**
    * @return Begin index of the external link.
    */
-  public int getLinkBeginIndex() {
-    return linkBeginIndex;
-  }
+  public int getLinkBeginIndex() { return linkBeginIndex; }
 
   /**
    * @return End index of the external link.
    */
-  public int getLinkEndIndex() {
-    return linkEndIndex;
-  }
+  public int getLinkEndIndex() { return linkEndIndex; }
 
   /**
    * @return Text.
    */
-  public String getText() {
-    return text;
-  }
+  public String getText() { return text; }
 
   /**
    * @return Text offset.
    */
-  public int getTextOffset() {
-    return textOffset;
-  }
+  public int getTextOffset() { return textOffset; }
 
   /**
    * @return Text.
    */
-  public String getTextNotTrimmed() {
-    return textNotTrimmed;
-  }
+  public String getTextNotTrimmed() { return textNotTrimmed; }
 
   /**
    * @return Displayed text.
@@ -281,20 +272,16 @@ public class PageElementExternalLink extends PageElement {
   /**
    * @return True if the link is in  [...]
    */
-  public boolean hasSquare() {
-    return hasSquare;
-  }
+  public boolean hasSquare() { return hasSquare; }
 
   /**
    * @return True if the link is in [...] and not in ([...[[...]]...])
    */
-  public boolean hasSecondSquare() {
-    return hasSecondSquare;
-  }
+  public boolean hasSecondSquare() { return hasSecondSquare; }
 
   /**
    * Constructor.
-   * 
+   *
    * @param beginIndex Begin index of the entire external link.
    * @param endIndex End index of the entire external link.
    * @param link Target of the link.
@@ -305,11 +292,10 @@ public class PageElementExternalLink extends PageElement {
    * @param hasSquare True if there's an open square bracket.
    * @param hasSecondSquare True if there's also an close square bracket.
    */
-  private PageElementExternalLink(
-      int beginIndex, int endIndex,
-      String link, int linkBeginIndex, int linkEndIndex,
-      String text, int textOffset,
-      boolean hasSquare, boolean hasSecondSquare) {
+  private PageElementExternalLink(int beginIndex, int endIndex, String link,
+                                  int linkBeginIndex, int linkEndIndex,
+                                  String text, int textOffset,
+                                  boolean hasSquare, boolean hasSecondSquare) {
     super(beginIndex, endIndex);
     this.linkNotTrimmed = link;
     String tmpLink = (link != null) ? link.trim() : null;
@@ -349,7 +335,7 @@ public class PageElementExternalLink extends PageElement {
 
   /**
    * Create an external link.
-   * 
+   *
    * @param link Link.
    * @param text Displayed text.
    * @return External link.
