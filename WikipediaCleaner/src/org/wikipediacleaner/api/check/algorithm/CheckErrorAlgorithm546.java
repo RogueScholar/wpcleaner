6856c7ede4b4fd383d7093d98b75fed4a2747ab7
Restyled.io <commits@restyled.io>
2019-12-05 15:40:54 +0000

    Restyled by clang-format

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm546.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm546.java
@@ -9,7 +9,6 @@ package org.wikipediacleaner.api.check.algorithm;
 
 import java.util.Collection;
 import java.util.List;
-
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.constants.WPCConfiguration;
 import org.wikipediacleaner.api.data.Namespace;
@@ -17,29 +16,27 @@ import org.wikipediacleaner.api.data.PageAnalysis;
 import org.wikipediacleaner.api.data.PageElementCategory;
 import org.wikipediacleaner.api.data.PageElementTemplate;
 
-
 /**
  * Algorithm for analyzing error 546 of check wikipedia project.
  * Error 546: Article without categories.
  */
 public class CheckErrorAlgorithm546 extends CheckErrorAlgorithmBase {
 
-  public CheckErrorAlgorithm546() {
-    super("Article without categories");
-  }
+  public CheckErrorAlgorithm546() { super("Article without categories"); }
 
   /**
    * Analyze a page to check if errors are present.
-   * 
+   *
    * @param analysis Page analysis.
    * @param errors Errors found in the page.
-   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
+   * @param onlyAutomatic True if analysis could be restricted to errors
+   *     automatically fixed.
    * @return Flag indicating if the error was found.
    */
   @Override
-  public boolean analyze(
-      PageAnalysis analysis,
-      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
+  public boolean analyze(PageAnalysis analysis,
+                         Collection<CheckErrorResult> errors,
+                         boolean onlyAutomatic) {
     if (analysis == null) {
       return false;
     }
@@ -65,10 +62,12 @@ public class CheckErrorAlgorithm546 extends CheckErrorAlgorithmBase {
     // Retrieve configuration
     String tmp = getSpecificProperty("templates", true, true, false);
     if ((tmp != null) && !tmp.isEmpty()) {
-      List<String> categorizingTemplates = WPCConfiguration.convertPropertyToStringList(tmp);
+      List<String> categorizingTemplates =
+          WPCConfiguration.convertPropertyToStringList(tmp);
       if (categorizingTemplates != null) {
         for (String categorizingTemplate : categorizingTemplates) {
-          List<PageElementTemplate> templates = analysis.getTemplates(categorizingTemplate);
+          List<PageElementTemplate> templates =
+              analysis.getTemplates(categorizingTemplate);
           if ((templates != null) && !templates.isEmpty()) {
             return false;
           }
