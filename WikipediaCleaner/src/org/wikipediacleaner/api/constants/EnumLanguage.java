df27506246c83a68e47682b419ade7ae709f045f
Restyled.io <commits@restyled.io>
2020-02-18 12:59:08 +0000

    Restyled by clang-format

--- WikipediaCleaner/src/org/wikipediacleaner/api/constants/EnumLanguage.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/constants/EnumLanguage.java
@@ -11,50 +11,48 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
-
 import org.wikipediacleaner.i18n.Messages;
-
 
 /**
  * Encapsulate possible languages.
  */
 public enum EnumLanguage {
 
-  AST  (new Locale("ast")     , "Asturian"),
-  AZ   (new Locale("az")      , "Azerbaijani"),
-  BN   (new Locale("bn")      , "Bengali"),
-  CKB  (new Locale("ckb")     , "Kurdish (Sorani)"),
-  CS   (new Locale("cs")      , "Czech"),
-  DA   (new Locale("da")      , "Danish"),
-  DE   (Locale.GERMAN         , "German"),
-  EL   (new Locale("el")      , "Greek"),
-  EN   (Locale.ENGLISH        , "English"),
-  EN_GB(new Locale("en_GB")   , "English (United Kingdom)"),
-  EO   (new Locale("eo")      , "Esperanto"),
-  ES   (new Locale("es")      , "Spanish"),
-  FA   (new Locale("fa")      , "Persian"),
-  FR   (Locale.FRENCH         , "Français"),
-  GL   (new Locale("gl")      , "Galician"),
-  HE   (new Locale("he")      , "Hebrew"),
-  HU   (new Locale("hu")      , "Hungarian"),
-  ID   (new Locale("id")      , "Indonesian"),
-  IS   (new Locale("is")      , "Icelandic"),
-  IT   (Locale.ITALIAN        , "Italian"),
-  JA   (Locale.JAPANESE       , "Japanese"),
-  KO   (Locale.KOREAN         , "Korean"),
-  MR   (new Locale("mr")      , "Marathi"),
-  MS   (new Locale("ms")      , "Malay"),
-  NB   (new Locale("nb")      , "Norwegian Bokmal"),
-  NL   (new Locale("nl")      , "Dutch"),
-  OC   (new Locale("oc")      , "Occitan"),
-  PL   (new Locale("pl")      , "Polish"),
-  PS   (new Locale("ps")      , "Pashto"),
+  AST(new Locale("ast"), "Asturian"),
+  AZ(new Locale("az"), "Azerbaijani"),
+  BN(new Locale("bn"), "Bengali"),
+  CKB(new Locale("ckb"), "Kurdish (Sorani)"),
+  CS(new Locale("cs"), "Czech"),
+  DA(new Locale("da"), "Danish"),
+  DE(Locale.GERMAN, "German"),
+  EL(new Locale("el"), "Greek"),
+  EN(Locale.ENGLISH, "English"),
+  EN_GB(new Locale("en_GB"), "English (United Kingdom)"),
+  EO(new Locale("eo"), "Esperanto"),
+  ES(new Locale("es"), "Spanish"),
+  FA(new Locale("fa"), "Persian"),
+  FR(Locale.FRENCH, "Français"),
+  GL(new Locale("gl"), "Galician"),
+  HE(new Locale("he"), "Hebrew"),
+  HU(new Locale("hu"), "Hungarian"),
+  ID(new Locale("id"), "Indonesian"),
+  IS(new Locale("is"), "Icelandic"),
+  IT(Locale.ITALIAN, "Italian"),
+  JA(Locale.JAPANESE, "Japanese"),
+  KO(Locale.KOREAN, "Korean"),
+  MR(new Locale("mr"), "Marathi"),
+  MS(new Locale("ms"), "Malay"),
+  NB(new Locale("nb"), "Norwegian Bokmal"),
+  NL(new Locale("nl"), "Dutch"),
+  OC(new Locale("oc"), "Occitan"),
+  PL(new Locale("pl"), "Polish"),
+  PS(new Locale("ps"), "Pashto"),
   PT_BR(new Locale("pt", "BR"), "Brazilian Portuguese"),
-  RU   (new Locale("ru")      , "Russian"),
-  SV   (new Locale("sv")      , "Swedish"),
-  TR   (new Locale("tr")      , "Turkish"),
-  UK   (new Locale("uk")      , "Ukrainian"),
-  UR   (new Locale("ur")      , "Urdu"),
+  RU(new Locale("ru"), "Russian"),
+  SV(new Locale("sv"), "Swedish"),
+  TR(new Locale("tr"), "Turkish"),
+  UK(new Locale("uk"), "Ukrainian"),
+  UR(new Locale("ur"), "Urdu"),
   ZH_CN(new Locale("zh", "CN"), "Chinese (Simplified)");
 
   private final String code;
@@ -75,7 +73,7 @@ public enum EnumLanguage {
       Class bundleClass = Class.forName(Messages.class.getName() + "_" + code);
       if ((bundleClass != null) &&
           (ResourceBundle.class.isAssignableFrom(bundleClass))) {
-        tmpBundle = (ResourceBundle) bundleClass.newInstance();
+        tmpBundle = (ResourceBundle)bundleClass.newInstance();
       }
     } catch (ClassNotFoundException e) {
       // No bundle, we simply don't have translation for this language
@@ -84,7 +82,8 @@ public enum EnumLanguage {
     } catch (InstantiationException e) {
       // Shouldn't happen, but same consequence as ClassNotFoundException
     } catch (UnsupportedClassVersionError e) {
-      // May happen if messages compiled with a more recent Java, but same consequence as ClassNotFoundException
+      // May happen if messages compiled with a more recent Java, but same
+      // consequence as ClassNotFoundException
     }
     bundle = tmpBundle;
   }
@@ -93,7 +92,8 @@ public enum EnumLanguage {
    * @return Vector of all languages.
    */
   public static List<EnumLanguage> getList() {
-    List<EnumLanguage> list = new ArrayList<EnumLanguage>(EnumLanguage.values().length);
+    List<EnumLanguage> list =
+        new ArrayList<EnumLanguage>(EnumLanguage.values().length);
     for (EnumLanguage e : EnumLanguage.values()) {
       list.add(e);
     }
@@ -131,30 +131,22 @@ public enum EnumLanguage {
   /**
    * @return Code.
    */
-  public String getCode() {
-    return code;
-  }
+  public String getCode() { return code; }
 
   /**
    * @return Locale.
    */
-  public Locale getLocale() {
-    return locale;
-  }
+  public Locale getLocale() { return locale; }
 
   /**
    * @return Language.
    */
-  public String getLanguage() {
-    return language;
-  }
+  public String getLanguage() { return language; }
 
   /**
    * @return Resource bundle for translation.
    */
-  public final ResourceBundle getResourceBundle() {
-    return bundle;
-  }
+  public final ResourceBundle getResourceBundle() { return bundle; }
 
   /* (non-Javadoc)
    * @see java.lang.Enum#toString()
